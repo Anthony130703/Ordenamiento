061543701fd42c17ec4346238dbb17a827bbfb63
Anthony <tonynarciso2003@gmail.com>
2024-10-14 18:09:17 -0500

    agregando el git

---
+++ arreglo.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include "burbuja.h"
+
+int main() {
+    int n;
+    std::cout << "Cuantos elementos deseas ingresar? ";
+    std::cin >> n;
+
+    int numeros[n];  
+
+    std::cout << "Ingresa " << n << " numeros:\n";
+    for (int i = 0; i < n; ++i) {
+        std::cin >> numeros[i];
+    }
+    std::cout<<"El arreglo es: ";
+     for (int i = 0; i < n; ++i) {
+        std::cout << numeros[i] << " ";
+    }
+    std::cout << std::endl;
+
+    // Ordenar el arreglo usando el método burbuja
+    burbuja(numeros, n);
+
+    std::cout << "Los numeros ordenados son: ";
+    for (int i = 0; i < n; ++i) {
+        std::cout << numeros[i] << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
---
+++ burbuja.h
@@ -0,0 +1,6 @@
+#ifndef BURBUJA_H
+#define BURBUJA_H
+
+void burbuja(int arr[], int n);
+
+#endif
